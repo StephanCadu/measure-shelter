01f1cf0758711d3c0a7544ed16be89681d3e611e
Cadu <cdauk@hotmail.com>
2023-07-07 17:33:12 -0300

    start: starting measure shelter

---
+++ src/main/java/agrotechfields/measureshelter/MeasureShelterApplication.java
@@ -0,0 +1,13 @@
+package agrotechfields.measureshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agrotechfields/measureshelter/MeasureShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package agrotechfields.measureshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
