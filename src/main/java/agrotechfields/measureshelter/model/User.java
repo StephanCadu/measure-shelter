d8a6cf730d232adf58cf1e893d5a3c238bb2eb35
Cadu <cdauk@hotmail.com>
2023-07-11 17:40:51 -0300

    feat: create User model

---
+++ src/main/java/agrotechfields/measureshelter/model/User.java
@@ -0,0 +1,78 @@
+package agrotechfields.measureshelter.model;
+
+import org.springframework.data.annotation.Id;
+import agrotechfields.measureshelter.utils.Role;
+
+public class User {
+
+  @Id
+  private int id;
+
+  private String username;
+
+  private String password;
+
+  private Role role;
+
+  private boolean active;
+
+  /**
+   * Default constructor.
+   */
+  public User() {}
+
+  /**
+   * Constructor with args.
+   * 
+   * @param username User name.
+   * @param password User password.
+   * @param role User role.
+   */
+  public User(String username, String password, Role role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+    this.active = true;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+}
