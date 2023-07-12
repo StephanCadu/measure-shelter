1b01093be2602b9911af2fd8cb406fe465da1adb
Cadu <cdauk@hotmail.com>
2023-07-12 10:25:40 -0300

    feat: change IsleRepository findByName method

--- src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
@@ -1,8 +1,11 @@
 package agrotechfields.measureshelter.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.Isle;
 
 public interface IsleRepository extends MongoRepository<Isle, Integer> {
+
+  @Query("{name:'?0'}")
   public Isle findByName(String name);
 }
