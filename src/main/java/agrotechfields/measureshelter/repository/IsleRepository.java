86c5690d82a8c2b7438c1dae833f8efa5b75fb53
Cadu <cdauk@hotmail.com>
2023-07-12 11:54:45 -0300

    fix: change IsleRepository findByName method return type

--- src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
@@ -1,5 +1,6 @@
 package agrotechfields.measureshelter.repository;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.Isle;
@@ -7,5 +8,5 @@ import agrotechfields.measureshelter.model.Isle;
 public interface IsleRepository extends MongoRepository<Isle, Integer> {
 
   @Query("{name:'?0'}")
-  public Isle findByName(String name);
+  public Optional<Isle> findByName(String name);
 }
