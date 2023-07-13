7bcb5b9f419136a7ae85f24e9b3e307476faf3ba
Cadu <cdauk@hotmail.com>
2023-07-13 17:26:08 -0300

    fix: change UserRepository findByUsername method return type

--- src/main/java/agrotechfields/measureshelter/repository/UserRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package agrotechfields.measureshelter.repository;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.User;
@@ -7,5 +8,5 @@ import agrotechfields.measureshelter.model.User;
 public interface UserRepository extends MongoRepository<User, Integer> {
 
   @Query("{username:'?0'}")
-  public User findByUsername(String username);
+  public Optional<User> findByUsername(String username);
 }
