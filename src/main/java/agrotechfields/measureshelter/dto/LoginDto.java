86145e8079c79a531c9c7d0b34b27f005e764d20
Cadu <cdauk@hotmail.com>
2023-07-13 16:47:32 -0300

    feat: create Login dto

---
+++ src/main/java/agrotechfields/measureshelter/dto/LoginDto.java
@@ -0,0 +1,62 @@
+package agrotechfields.measureshelter.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import agrotechfields.measureshelter.utils.Role;
+
+public class LoginDto {
+
+  @NotNull
+  @NotBlank
+  private String username;
+
+  @NotNull
+  @NotBlank
+  @Size(min = 6)
+  private String password;
+
+  private Role role;
+
+  /**
+   * Default constructor.
+   */
+  public LoginDto() {}
+
+  /**
+   * Constructor with args.
+   * 
+   * @param username User name.
+   * @param password User password.
+   * @param role User role.
+   */
+  public LoginDto(String username, String password) {
+    this.username = username;
+    this.password = password;
+    this.role = Role.USER;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+}
