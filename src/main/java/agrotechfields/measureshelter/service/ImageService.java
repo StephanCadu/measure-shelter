c528cf2058edc26713d409e22ad1544d356d801d
Cadu <cdauk@hotmail.com>
2023-07-13 17:40:36 -0300

    feat: create Image service

---
+++ src/main/java/agrotechfields/measureshelter/service/ImageService.java
@@ -0,0 +1,52 @@
+package agrotechfields.measureshelter.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import agrotechfields.measureshelter.dto.ImageDto;
+import agrotechfields.measureshelter.model.Image;
+import agrotechfields.measureshelter.repository.ImageRepository;
+
+public class ImageService {
+
+  @Autowired
+  private ImageRepository imageRepository;
+
+  public List<Image> findImages() {
+    return this.imageRepository.findAll();
+  }
+
+  public Image findImageById(int imageId) {
+    Optional<Image> imageFound = this.imageRepository.findById(imageId);
+
+    if (imageFound.isEmpty()) {
+      // throw ObjectNotFoundException
+    }
+
+    return imageFound.get();
+  }
+
+  public Image findImageByName(String imageName) {
+    Optional<Image> imageFound = this.imageRepository.findByName(imageName);
+
+    if (imageFound.isEmpty()) {
+      // throw ObjectNotFoundException
+    }
+
+    return imageFound.get();
+  }
+
+  public Image saveImage(ImageDto imageDto) {
+    Optional<Image> imageFound = this.imageRepository.findByName(imageDto.getName());
+
+    if (imageFound.isPresent()) {
+      // throw ObjectAlreadyExistsException
+    }
+
+    Image image = new Image();
+    image.setName(imageDto.getName());
+    image.setBytes(imageDto.getBytes());
+
+    return this.imageRepository.insert(image);
+  }
+}
