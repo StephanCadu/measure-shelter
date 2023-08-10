6bad27ddfab88701a1cba6f402212976559cfe80
Cadu <cdauk@hotmail.com>
2023-08-10 19:14:19 -0300

    fix: change id type on Image repository

--- src/main/java/agrotechfields/measureshelter/repository/ImageRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/ImageRepository.java
@@ -1,11 +1,12 @@
 package agrotechfields.measureshelter.repository;
 
 import java.util.Optional;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.Image;
 
-public interface ImageRepository extends MongoRepository<Image, Integer> {
+public interface ImageRepository extends MongoRepository<Image, ObjectId> {
 
   @Query("{name:'?0'}")
   public Optional<Image> findByName(String name);
