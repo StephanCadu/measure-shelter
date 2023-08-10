71fb84edd769acace7e5d9ff7721b8a7f2adc000
Cadu <cdauk@hotmail.com>
2023-08-10 19:14:34 -0300

    fix: change id type on Isle repository

--- src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/IsleRepository.java
@@ -1,11 +1,12 @@
 package agrotechfields.measureshelter.repository;
 
 import java.util.Optional;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.Isle;
 
-public interface IsleRepository extends MongoRepository<Isle, Integer> {
+public interface IsleRepository extends MongoRepository<Isle, ObjectId> {
 
   @Query("{name:'?0'}")
   public Optional<Isle> findByName(String name);
