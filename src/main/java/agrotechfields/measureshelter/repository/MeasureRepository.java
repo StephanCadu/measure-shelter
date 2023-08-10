bfd447181f53a35c83ff92e8df61bdcb92dce882
Cadu <cdauk@hotmail.com>
2023-08-10 19:14:48 -0300

    fix: change id type on Measure repository

--- src/main/java/agrotechfields/measureshelter/repository/MeasureRepository.java
+++ src/main/java/agrotechfields/measureshelter/repository/MeasureRepository.java
@@ -1,11 +1,12 @@
 package agrotechfields.measureshelter.repository;
 
 import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import agrotechfields.measureshelter.model.Measure;
 
-public interface MeasureRepository extends MongoRepository<Measure, Integer> {
+public interface MeasureRepository extends MongoRepository<Measure, ObjectId> {
 
   @Query("{isleId:'?0'}")
   public List<Measure> findByIsleId(int isleId);
