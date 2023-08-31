c04d30d95fb2741118b43a016096ec45f7116ff7
Cadu <cdauk@hotmail.com>
2023-08-31 17:54:33 -0300

    fix: change return types to MeasureResponse on Measure controller

--- src/main/java/agrotechfields/measureshelter/controller/MeasureController.java
+++ src/main/java/agrotechfields/measureshelter/controller/MeasureController.java
@@ -1,7 +1,7 @@
 package agrotechfields.measureshelter.controller;
 
 import java.util.List;
-import javax.annotation.security.RolesAllowed;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import agrotechfields.measureshelter.dto.MeasureDto;
+import agrotechfields.measureshelter.dto.MeasureResponse;
 import agrotechfields.measureshelter.model.Measure;
 import agrotechfields.measureshelter.service.MeasureService;
 
@@ -27,40 +28,43 @@ public class MeasureController {
   private MeasureService measureService;
 
   @GetMapping
-  public ResponseEntity<List<Measure>> findMeasures() {
-    return ResponseEntity.status(HttpStatus.OK).body(this.measureService.findMeasures());
+  public ResponseEntity<List<MeasureResponse>> findMeasures() {
+    List<MeasureResponse> measures = this.measureService.findMeasures().stream()
+        .map(measure -> new MeasureResponse(measure)).collect(Collectors.toList());
+    return ResponseEntity.status(HttpStatus.OK).body(measures);
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Measure> findMeasureById(@PathVariable("id") String measureId) {
+  public ResponseEntity<MeasureResponse> findMeasureById(@PathVariable("id") String measureId) {
     Measure measure = this.measureService.findMeasureById(new ObjectId(measureId));
-    return ResponseEntity.status(HttpStatus.OK).body(measure);
+    return ResponseEntity.status(HttpStatus.OK).body(new MeasureResponse(measure));
   }
 
   @GetMapping("/byisle/{id}")
-  public ResponseEntity<List<Measure>> findMeasuresByIsle(@PathVariable("id") int isleId) {
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(this.measureService.findMeasuresByIsle(isleId));
+  public ResponseEntity<List<MeasureResponse>> findMeasuresByIsle(
+      @PathVariable("id") String isleId) {
+
+    List<MeasureResponse> measures = this.measureService.findMeasuresByIsle(isleId).stream()
+        .map(measure -> new MeasureResponse(measure)).collect(Collectors.toList());
+    return ResponseEntity.status(HttpStatus.OK).body(measures);
   }
 
   @PostMapping
-  public ResponseEntity<Measure> saveMeasure(@Valid @RequestBody MeasureDto measureDto) {
+  public ResponseEntity<MeasureResponse> saveMeasure(@Valid @RequestBody MeasureDto measureDto) {
     Measure measure = this.measureService.saveMeasure(measureDto);
-    return ResponseEntity.status(HttpStatus.CREATED).body(measure);
+    return ResponseEntity.status(HttpStatus.CREATED).body(new MeasureResponse(measure));
   }
 
   @DeleteMapping("/{id}")
-  @RolesAllowed("ADMIN")
   public ResponseEntity<String> deleteMeasure(@PathVariable("id") String measureId) {
     this.measureService.deleteMeasure(new ObjectId(measureId));
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
   @PutMapping("/{id}")
-  @RolesAllowed("ADMIN")
-  public ResponseEntity<Measure> updateMeasure(@PathVariable("id") String measureId,
+  public ResponseEntity<MeasureResponse> updateMeasure(@PathVariable("id") String measureId,
       @Valid @RequestBody MeasureDto measureDto) {
     Measure measure = this.measureService.updateMeasure(new ObjectId(measureId), measureDto);
-    return ResponseEntity.status(HttpStatus.ACCEPTED).body(measure);
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(new MeasureResponse(measure));
   }
 }
